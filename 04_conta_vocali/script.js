ab28f629e985a0f626041bcf5fe5d604b566a483
dpalexandru <alexpoian91@hotmail.com>
2025-07-08 16:12:12 +0200

    feat: function forcount the vowels

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,6 +5,23 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const contaVocali = (stringa) => {
+  let numeroVocali 
+  for (i=0; i<stringa.length; i++)
+    if (stringa[i] === a)
+    numeroVocali++;
+  else if (stringa[i] === e) {
+    numeroVocali++
+  }else if (stringa[i] === u) {
+    numeroVocali++
+  } else if (stringa[i] === o) {
+    numeroVocali++
+  } else if (stringa[i] === i) {
+    numeroVocali++
+  }
+  return numeroVocali;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
