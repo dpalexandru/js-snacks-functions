62265a8b65b784f806d79cefd916feac574d0c50
dpalexandru <alexpoian91@hotmail.com>
2025-07-08 15:26:16 +0200

    feat: first function for first char

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,9 +1,15 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const iniziali= [];
 
 // Dichiara la funzione qui.
+const soloIniziali = (nomi) => {
+  for (i=0; i<nomi.lenght; i++)[
+    let primaLettera = nomi[i].charAt(0);
+    iniziali.push(primaLettera);
+  ]
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
