9a24366a4a021eea23446ed4ec1a165d45cc3539
dpalexandru <alexpoian91@hotmail.com>
2025-07-08 16:19:05 +0200

    fix: correct scope use for variables

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,21 +1,23 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-const iniziali= [];
 
 // Dichiara la funzione qui.
 const soloIniziali = (nomi) => {
+  const iniziali= [];
+
   for (i=0; i<nomi.length; i++) {
     let primaLettera = nomi[i].charAt(0);
     iniziali.push(primaLettera);
   }
+  return iniziali;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-soloIniziali(names);
+const result = soloIniziali(names);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
 
 console.log(`Array originale: ${names}`)
-console.log(`Solo iniziali: ${iniziali}`)
+console.log(`Solo iniziali: ${result}`)
