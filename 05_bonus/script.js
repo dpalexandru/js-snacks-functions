8026ad4f34d31c7c4d4008c6f5106d4098d6be96
dpalexandru <alexpoian91@hotmail.com>
2025-07-08 16:52:29 +0200

    feat: function to print customizable hello

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,33 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const name = 'Alex';
 
+console.log ('------Orario Modificabile------')
 
 // Dichiara la funzione qui.
 
+const saluto = (nome , orario) => {
+  if (orario >= 13 && orario <= 17) {
+    console.log (`Buon pomeriggio ${nome}`)
+  } else if (orario < 13) {
+    console.log (`Buongiorno ${nome}`)
+  } else {
+    console.log (`Buonasera ${nome}`)
+  }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const ora = 14; 
+saluto(name, ora); 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+// Prova con orario vero 
+
+console.log ('------Orario Vero------')
+const oraVera = new Date().getHours();
+console.log (`Ora sono le: ${oraVera} `)
+saluto(name, oraVera); 
+
+
