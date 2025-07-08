a3dd3fd2dc38414ceee29fe6e16f24d1783bfc4f
dpalexandru <alexpoian91@hotmail.com>
2025-07-08 15:53:40 +0200

    feat: new fuction for search in array

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,9 +1,20 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const flitro = "a"
 
 // Dichiara la funzione qui.
+
+const filtraggioIniziali = (nomi, iniziale) => {
+  let filtroIniziali = [];
+ 
+   for (i=0; nomi.length; i++);{
+    if (iniziale = nomi[i].charAt(0)){
+      filtroIniziali = nomi[i];
+    }
+   }
+  
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
